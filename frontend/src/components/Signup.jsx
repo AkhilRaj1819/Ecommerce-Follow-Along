6e8a27a0ea9f4f84cc1bab1d960e77dfde764d95
AkhilRaj1819 <vysyaraju.akhilraju.s89@kalvium.community>
2025-03-13 15:26:53 +0530

    13e

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,56 +1,58 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import './Signup.css';
 
+const Signup = () => {
+  const [userDetail, setUserDetail] = useState({
+    name: '',
+    email: '',
+    password: ''
+  });
 
-const Signup = () =>{
-    const [userDetail, setUserDetail] = useState({
-        name: '',
-        email: '',
-        password: ''
-});
-    function handelInput(event){
-        console.log(event.target.value);
-        setUserDetail({...userDetail,[event.target.value]:event.target.value});
+  function handleInput(e) {
+    setUserDetail({ ...userDetail, [e.target.name]: e.target.value });
+  }
 
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (userDetail.name === '') {
+      alert('Please enter your name');
+      return;
     }
-    async function handelSubmit(){
-        try{
-            if(userDetail.name == ''){
-                alert('Please enter your name')
-                return;
-            }
-            if(userDetail.email == ''){
-                alert('Please enter your email')
-                return;
-            }
-            if(userDetail.password == ''){
-                alert('Please enter your password')
-                return;
-            }
-            const data= await axios.post("https://localhost:8000/user/signup");
-            console.log(data);
-            alert('signup successful')
-
-        }catch(err){
-            console.log(err);
-            alert('something went wrong')
-        }
-
+    if (userDetail.email === '') {
+      alert('Please enter your email');
+      return;
     }
-    return (
-        <div className='signup'>
-            <h1>Sign up</h1>
-            <form action="" onSubmit={handelSubmit}>
-            <label htmlFor=''>Name</label>
-            <input type='text' name='name' placeholder='Name...' onChange={handelInput}/>
-            <label htmlFor=''>Email</label>
-            <input type='email' name='email' placeholder='Email...' onChange={handelInput}/>
-            <label htmlFor=''>Password</label>
-            <input type='password' name='password' placeholder='Password...' onChange={handelInput} />
-            <input type='submit' />
-            </form>
-        </div>
-    )
-}
+    if (userDetail.password === '') {
+      alert('Please enter your password');
+      return;
+    }
+
+    try {
+      const response = await axios.post("http://localhost:8000/user/signup", userDetail);
+      console.log('Response:', response.data);
+      alert('Signup successful');
+    } catch (err) {
+      console.error("Error during signup:", err);
+      alert('Something went wrong');
+    }
+  }
+
+  return (
+    <div className='signup'>
+      <h1>Sign up</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input type='text' name='name' placeholder='Name...' onChange={handleInput} />
+        <label htmlFor="email">Email</label>
+        <input type='email' name='email' placeholder='Email...' onChange={handleInput} />
+        <label htmlFor="password">Password</label>
+        <input type='password' name='password' placeholder='Password...' onChange={handleInput} />
+        <input type='submit' value="Sign Up" />
+      </form>
+    </div>
+  );
+};
+
 export default Signup;
